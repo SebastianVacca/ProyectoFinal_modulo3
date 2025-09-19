e3cd38420db73aafe80966b065e58787cafd3273
Juan Vacca <clanbs11.12@gmail.com>
2025-09-18 21:08:39 -0500

    elimino linea de import sin usar en el archivo CursoServiceTest.java

--- cursos/src/test/java/com/plataforma/service/CursoServiceTest.java
+++ cursos/src/test/java/com/plataforma/service/CursoServiceTest.java
@@ -1,6 +1,5 @@
 package com.plataforma.service;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
